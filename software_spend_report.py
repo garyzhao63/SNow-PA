968c669b6bb25872daa459cb83e249d6127a70b0
jiahao.zhao <jiahao.zhao@servicenow.com>
2019-02-04 15:07:02 -0800

    switched from dict of list to dict of dict

--- software_spend_report.py
+++ software_spend_report.py
@@ -1,18 +1,23 @@
+from collections import defaultdict
 import sys
 import csv
 
 #command line argument 
 fName = sys.argv[1]
 
-vendorDict = {}
+vendorDict = defaultdict(dict)
+#vendorDict = {}
 
 with open(fName) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         currVendor = row['Vendor']
-        vendorDict[currVendor] = 
-        print vendorDict[currVendor]
+        currProduct = row['Product']
+
+        print row['Amount']
+
+        vendorDict[currVendor][currProduct] = row['Amount']
 
 
-         
+print vendorDict
 
