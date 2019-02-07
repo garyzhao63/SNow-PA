9f5b36e0eb9ca8d5ec1dc885975293d1efcfa1f6
jiahao.zhao <jiahao.zhao@servicenow.com>
2019-02-06 21:06:58 -0800

    added bash script to test expected_output files

--- software_spend_report.py
+++ software_spend_report.py
@@ -3,7 +3,7 @@ import sys
 import csv
 import math
 
-# Function to format a number to dollar format e.g. 1234 -> $1,234
+# Function to format a number to its dollar format e.g. 1234 -> $1,234
 def numToDollars(amount):
     return str('${:,}'.format(amount))
 
@@ -11,41 +11,37 @@ def numToDollars(amount):
 def truncateDecimals(amount):
     return math.trunc(amount)
 
-# Function to parse a row and insert it into the dict
-def insertRowToDict(row):
-    currVendor = row['Vendor']
-    currProduct = row['Product']
-
-    #add the spending to the current spending
-    vendorDict[currVendor][currProduct] += float(row['Amount'])
-
-###############################################################
+####################################################################
 
 #File name from command line
 fName = sys.argv[1]
 
-# Initialize vendorDict to a dict of dict, 
-# and initialize each dict in vendorDict to a dict of int
+# Initialize vendorDict as a dict of dict and
+# Initialize each dict in vendorDict as a dict of float
 vendorDict = defaultdict(lambda: defaultdict(float))
 
 with open(fName) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        insertRowToDict(row)
+        currVendor = row['Vendor']
+        currProduct = row['Product']
 
-#Loop through vendorDict in alphabetical order
+        # add the spending of the same product to that product's total spending
+        vendorDict[currVendor][currProduct] += float(row['Amount'])
+
+## Loop through vendorDict in alphabetical order
 for vendor, productDict in sorted(vendorDict.items()):
     vendorAmount = 0
     vendorStr = '' 
     productStr = ''
 
-    #Loop through productDict in alphabetical order
-    for product, amount in sorted(productDict.items()):
-        vendorAmount += amount;
+    ## Loop through productDict in alphabetical order
+    for product, productAmount in sorted(productDict.items()):
+        # Add the product spending to the vendor's total spending
+        vendorAmount += productAmount;
         productStr += '  ' + product + ' ' + numToDollars(truncateDecimals(amount)) + '\n'
-
 
     vendorStr = vendor + ' ' + numToDollars(truncateDecimals(vendorAmount))
 
-    #rstrip removes the last new line
+    # rstrip() removes the last new line
     print (vendorStr + '\n' + productStr.rstrip())
